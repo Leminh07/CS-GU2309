6b7e6a684e6b4f41786def1075f6f2956d83ee1f
Leminh07 <147058853+Leminh07@users.noreply.github.com>
2023-10-12 17:19:55 +0700

    .

---
+++ B02/Xinchao/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace xinchao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LeMinh");
+           
+            Console.WriteLine("Hello: Minh" );
+        }
+    }
+}
