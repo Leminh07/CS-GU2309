3c2f347d0e42c8438d7b3ee2e48d8f3509c2b97a
Leminh07 <147058853+Leminh07@users.noreply.github.com>
2023-10-12 16:53:22 +0700

    .

---
+++ Hello/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime localDate = DateTime.Now;
+        System.Console.WriteLine("Datetime Now is :" + localDate);
+    }
+}
